78bf1b77df3bede7843ed6ecd8c695b141e76ebe
joseuilton <joseuilton.siqueira@gmail.com>
2024-02-01 22:06:48 -0300

    chore: structuring global styles

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Teste técnico para a legaplan",
+  description: "Aplicativo de lista de tarefas criado com Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
