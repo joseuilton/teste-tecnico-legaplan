e7fe8ffb5d5baab4b451e523add2a89c94d3cc9f
joseuilton <joseuilton.siqueira@gmail.com>
2024-02-03 12:04:33 -0300

    feat: add responsivity to Header component

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -1,12 +1,24 @@
 import Image from "next/image"
 import styles from "./styles.module.css"
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 const Header = () => {
     return (
         <header className={styles.header}>
-            <Image src="/logo.png" alt="Logo FocalPoint" width={150} height={36} />
-            <h1 className={styles.header__title}>Bem vindo de volta, Marcus</h1>
-            <time className={styles.header__timeInfo}>Segunda, 22 de dezembro de 2024</time>
+            <Image
+                className={styles.header__logo}
+                src="/logo.png"
+                alt="Logo FocalPoint"
+                width={150}
+                height={36}
+            />
+            <h1 className={styles.header__title}>Bem vindo de volta!</h1>
+            <time className={styles.header__timeInfo}>
+                {format(new Date(), "EEEE',' dd 'de' MMMM 'de' yyyy", {
+                    locale: ptBR
+                })}
+            </time>
         </header>
     )
 }
